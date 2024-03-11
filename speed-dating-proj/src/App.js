c07b8fa9506b75e146b3438bb4f9fd920feecbb4
noa-co <noa1999@windowslive.com>
2024-03-11 12:46:53 +0200

    trying to fix routing..

--- speed-dating-proj/src/App.js
+++ speed-dating-proj/src/App.js
@@ -13,7 +13,7 @@ import { getCurrentUser } from 'aws-amplify/auth';
 import LandingPage from "./components/login-page/landing-page/landing-page";
 import ComfortingUserScreen from "./components/video-chat/comforting-user-screen/comforting-user-screen";
 import { Amplify } from 'aws-amplify';
-import config from './aws-exports.js';
+import config from './amplifyconfiguration.json';
 
 Amplify.configure(config);
 
